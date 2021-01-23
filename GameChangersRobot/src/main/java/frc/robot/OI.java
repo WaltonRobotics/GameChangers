47a6745829ac3149c5278998fd171066c06e1e7f
bhavika-byte <bhavika.thadani@waltonrobotics.org>
2021-01-23 11:19:27 -0500

    Drive Command (has errors to fix)

--- GameChangersRobot/src/main/java/frc/robot/OI.java
+++ GameChangersRobot/src/main/java/frc/robot/OI.java
@@ -1,5 +1,14 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import static frc.robot.Constants.Inputs.LEFT_JOYSTICK_PORT;
+import static frc.robot.Constants.Inputs.RIGHT_JOYSTICK_PORT;
+
+
 public class OI {
 
+    public static Joystick leftJoystick = new Joystick(LEFT_JOYSTICK_PORT);
+    public static Joystick rightJoystick = new Joystick(RIGHT_JOYSTICK_PORT);
 }
--- GameChangersRobot/src/main/java/frc/robot/commands/teleop/DriveCommand.java
+++ GameChangersRobot/src/main/java/frc/robot/commands/teleop/DriveCommand.java
@@ -1,4 +1,33 @@
 package frc.robot.commands.teleop;
 
-public class DriveCommand {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import static frc.robot.Robot.driveTrain;
+import static frc.robot.OI.leftJoystick;
+import static frc.robot.OI.rightJoystick;
+import static frc.robot.Robot.driveTrain;
+
+public class DriveCommand extends CommandBase {
+
+    public DriveCommand() {
+        addRequirements(driveTrain);
+    }
+
+    public double getLeftJoystickY() {
+        return -leftJoystick.getY();
+    }
+
+    public double getRightJoystickY() {
+        return -rightJoystick.getY();
+    }
+
+    @Override
+    public void execute() {
+        driveTrain.setDutyCycles(getLeftJoystickY(), getRightJoystickY());
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
