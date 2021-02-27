c58f2fc5c65c8cc5b6f4862a0bea092c26441017
LAPTOP-Q4MNPLQ9\oscar <oscar1149412@gmail.com>
2021-02-27 14:44:33 -0500

    Created TurretCommand

--- GameChangersRobot/src/main/java/frc/robot/commands/teleop/TurretCommand.java
+++ GameChangersRobot/src/main/java/frc/robot/commands/teleop/TurretCommand.java
@@ -1,7 +1,29 @@
 package frc.robot.commands.teleop;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.stateMachine.IState;
+import frc.robot.subsystems.Turret;
+
 import static frc.robot.Robot.turret;
 
-public class TurretCommand {
+public class TurretCommand extends CommandBase {
+    public TurretCommand() {addRequirements(turret);}
+
+    IState mManual = new IState() {
+        @Override
+        public void initialize() {
+
+        }
+
+        @Override
+        public IState execute() {
+            return null;
+        }
+
+        @Override
+        public void finish() {
+
+        }
+    }
 
 }
