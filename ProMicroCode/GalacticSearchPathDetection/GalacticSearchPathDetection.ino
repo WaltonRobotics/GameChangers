fb4845fce80bcb4598fb74f6260ce0cd5c761cd2
Vedika Bhatnagar <52089706+vedikat@users.noreply.github.com>
2021-03-15 20:38:23 -0400

    Updated pixy data writing

--- ProMicroCode/GalacticSearchPathDetection/GalacticSearchPathDetection.ino
+++ ProMicroCode/GalacticSearchPathDetection/GalacticSearchPathDetection.ino
@@ -16,11 +16,15 @@
 #define GALACTIC_SEARCH_BLUE_B_DUTY_CYCLE 1.0
 
 #define NUM_BALLS_THRESHOLD 2
-#define MATCH_SCORE_THRESHOLD 15.0
+#define MATCH_SCORE_THRESHOLD 50
 
-#define LED_STRIP_READ_PIN 8
-#define PIXY_CAM_WRITE_PIN 9
+#define NO_DETERMINATION_BYTE_MESSAGE 0x0A
+#define GALACTIC_SEARCH_RED_A_BYTE_MESSAGE 0x0B
+#define GALACTIC_SEARCH_RED_B_BYTE_MESSAGE 0x0C
+#define GALACTIC_SEARCH_BLUE_A_BYTE_MESSAGE 0x0D
+#define GALACTIC_SEARCH_BLUE_B_BYTE_MESSAGE 0x0E
 
+#define BAUD_RATE 9600
 struct Target {
   int x;
   int y;
@@ -53,7 +57,7 @@ class PathID {
       this->matchScore = 0;
       this->name = "";
     }
-    
+
     PathID(String name) {
       this->nominalRegions = new Target[REGIONS_PER_PATH];
       this->matchScore = 0;
@@ -63,15 +67,15 @@ class PathID {
     ~PathID() {
       delete nominalRegions;
     }
-    
+
   public:
     Target *nominalRegions;
     float matchScore;
     String name;
-    float dutyCycle;
+    byte byteMessage;
 };
 
-// This is the main Pixy object 
+// This is the main Pixy object
 Pixy2 pixy;
 
 PathID galacticSearchPathIDs[NUM_PATHS];
@@ -82,90 +86,95 @@ PathID &getBestPath() {
 
   for (int i = 0; i < NUM_PATHS; i++) {
     float currentScore = galacticSearchPathIDs[i].matchScore;
-    
+
     if (currentScore < bestScore) {
       bestScore = currentScore;
       bestIndex = i;
     }
   }
-  
+
   return galacticSearchPathIDs[bestIndex];
 }
 
 void setup() {
-  Serial.begin(115200);
-  
+  Serial.begin(BAUD_RATE);
+
   pixy.init();
 
-  pinMode(LED_STRIP_READ_PIN, INPUT);
-  pinMode(PIXY_CAM_WRITE_PIN, OUTPUT);
-
   galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].name = "Galactic Search Red A";
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].nominalRegions[0] = Target(164, 122, 44, 40);
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].nominalRegions[1] = Target(246, 46, 20, 17);
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].nominalRegions[2] = Target(32, 44, 12, 6);
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].dutyCycle = GALACTIC_SEARCH_RED_A_DUTY_CYCLE;
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].nominalRegions[0] = Target(178, 120, 44, 38);
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].nominalRegions[1] = Target(251, 38, 22, 18);
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].nominalRegions[2] = Target(34, 39, 12, 10);
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_A_INDEX].byteMessage = GALACTIC_SEARCH_RED_A_BYTE_MESSAGE;
 
   galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].name = "Galactic Search Red B";
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].nominalRegions[0] = Target(250, 49, 20, 16);
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].nominalRegions[1] = Target(104, 32, 12, 9);
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].nominalRegions[2] = Target(2, 134, 4, 8);
-  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].dutyCycle = GALACTIC_SEARCH_RED_B_DUTY_CYCLE;
-  
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].nominalRegions[0] = Target(251, 39, 22, 16);
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].nominalRegions[1] = Target(109, 25, 14, 9);
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].nominalRegions[2] = Target(6, 131, 12, 8);
+  galacticSearchPathIDs[GALACTIC_SEARCH_RED_B_INDEX].byteMessage = GALACTIC_SEARCH_RED_B_BYTE_MESSAGE;
+
   galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].name = "Galactic Search Blue A";
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].nominalRegions[0] = Target(293, 32, 14, 13);
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].nominalRegions[1] = Target(109, 33, 10, 8);
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].nominalRegions[2] = Target(158, 19, 8, 7);
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].dutyCycle = GALACTIC_SEARCH_BLUE_A_DUTY_CYCLE;
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].nominalRegions[0] = Target(297, 24, 14, 12);
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].nominalRegions[1] = Target(108, 26, 12, 9);
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].nominalRegions[2] = Target(160, 12, 8, 8);
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_A_INDEX].byteMessage = GALACTIC_SEARCH_BLUE_A_BYTE_MESSAGE;
 
   galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].name = "Galactic Search Blue B";
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].nominalRegions[0] = Target(227, 35, 14, 12);
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].nominalRegions[1] = Target(114, 25, 12, 8);
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].nominalRegions[2] = Target(193, 14, 6, 6);
-  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].dutyCycle = GALACTIC_SEARCH_BLUE_B_DUTY_CYCLE;
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].nominalRegions[0] = Target(232, 28, 16, 12);
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].nominalRegions[1] = Target(116, 19, 12, 9);
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].nominalRegions[2] = Target(195, 6, 6, 7);
+  galacticSearchPathIDs[GALACTIC_SEARCH_BLUE_B_INDEX].byteMessage = GALACTIC_SEARCH_BLUE_B_BYTE_MESSAGE;
 }
 
 void loop() {
   const int numBallsDetected = pixy.ccc.getBlocks();
 
-  if (numBallsDetected)
-  {
-    for (PathID &path : galacticSearchPathIDs) {
-      path.matchScore = 0;
-      
-      for (int i = 0; i < numBallsDetected; i++)
-      {
-        Target blockTarget = Target(pixy.ccc.blocks[i].m_x, 
-                                    pixy.ccc.blocks[i].m_y,
-                                    pixy.ccc.blocks[i].m_width,
-                                    pixy.ccc.blocks[i].m_height);
-        
-        float bestScore = FLT_MAX;
-
-        for (int j = 0; j < REGIONS_PER_PATH; j++) {
-          float currentScore = blockTarget.getDistanceTo(path.nominalRegions[j]);
-          
-          if (currentScore < bestScore) {
-            bestScore = currentScore;
-          }
-        }
-
-        path.matchScore += bestScore;
-      }
-    }
-
-    PathID &bestPath = getBestPath();
-    Serial.println(bestPath.name + " - score: " + bestPath.matchScore);
+  if(Serial.available()) {
     
-    if (bestPath.matchScore < MATCH_SCORE_THRESHOLD && numBallsDetected >= NUM_BALLS_THRESHOLD) {
-      // digitalWrite(PIXY_CAM_WRITE_PIN, HIGH);
-      // analogWrite(PIXY_CAM_WRITE_PIN, (int)(bestPath.dutyCycle * 255.0));
-    } else {
+    byte value = Serial.read();
+    
+    if (numBallsDetected)
+    {
+      for (PathID &path : galacticSearchPathIDs) {
+        path.matchScore = 0;
+  
+        for (int i = 0; i < numBallsDetected; i++)
+        {
+          Target blockTarget = Target(pixy.ccc.blocks[i].m_x,
+                                      pixy.ccc.blocks[i].m_y,
+                                      pixy.ccc.blocks[i].m_width,
+                                      pixy.ccc.blocks[i].m_height);
+  
+          float bestScore = FLT_MAX;
+  
+          for (int j = 0; j < REGIONS_PER_PATH; j++) {
+            float currentScore = blockTarget.getDistanceTo(path.nominalRegions[j]);
+  
+            if (currentScore < bestScore) {
+              bestScore = currentScore;
+            }
+          }
+  
+          path.matchScore += bestScore;
+        }
+      }
+  
+      PathID &bestPath = getBestPath();
+      //Serial.println(bestPath.name + " - score: " + bestPath.matchScore);
+      
+      if (bestPath.matchScore < MATCH_SCORE_THRESHOLD && numBallsDetected >= NUM_BALLS_THRESHOLD) {
+        Serial.write(bestPath.byteMessage);
+        // digitalWrite(PIXY_CAM_WRITE_PIN, HIGH);
+        // analogWrite(PIXY_CAM_WRITE_PIN, (int)(bestPath.dutyCycle * 255.0));
+      } else {
+        Serial.write(NO_DETERMINATION_BYTE_MESSAGE);
+      } 
       // analogWrite(PIXY_CAM_WRITE_PIN, (int)(NO_DETERMINATION_DUTY_CYCLE * 255.0));
+    } else {
+      Serial.write(NO_DETERMINATION_BYTE_MESSAGE);
     }
-  } else {
-    // analogWrite(PIXY_CAM_WRITE_PIN, (int)(NO_DETERMINATION_DUTY_CYCLE * 255.0));
   }
 
-  analogWrite(PIXY_CAM_WRITE_PIN, (int)(0.25 * 255.0));
+  delay(50);
+
 }
