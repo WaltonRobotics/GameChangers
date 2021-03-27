4db952cbabae6d030fa0d332273a09ca265308dc
vmalepati1 <malepativikas@gmail.com>
2021-03-26 21:09:12 -0400

    Use limelight toggle

--- GameChangersRobot/src/main/java/frc/robot/commands/auton/AutoAlign.java
+++ GameChangersRobot/src/main/java/frc/robot/commands/auton/AutoAlign.java
@@ -1,21 +1,15 @@
 package frc.robot.commands.auton;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.vision.LimelightHelper;
 
-import static frc.robot.Constants.Shooter.kLimelightLEDWaitTimeSeconds;
 import static frc.robot.Robot.sDrivetrain;
 
 public class AutoAlign extends SequentialCommandGroup {
 
     public AutoAlign() {
         addCommands(
-                new InstantCommand(() -> LimelightHelper.setLEDMode(true)),
-                new WaitCommand(kLimelightLEDWaitTimeSeconds),
-                new TurnToAngle(() -> sDrivetrain.getHeading().getDegrees() - LimelightHelper.getTX()),
-                new InstantCommand(() -> LimelightHelper.setLEDMode(false))
+                new TurnToAngle(() -> sDrivetrain.getHeading().getDegrees() - LimelightHelper.getTX())
         );
     }
 
--- GameChangersRobot/src/main/java/frc/robot/commands/auton/shootingChallenges/CalibratePose.java
+++ GameChangersRobot/src/main/java/frc/robot/commands/auton/shootingChallenges/CalibratePose.java
@@ -3,24 +3,19 @@ package frc.robot.commands.auton.shootingChallenges;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.utils.DebuggingLog;
 import frc.robot.vision.LimelightHelper;
 import frc.robot.vision.PnPHelper;
 
 import java.util.logging.Level;
 
-import static frc.robot.Constants.Shooter.kLimelightLEDWaitTimeSeconds;
 import static frc.robot.Robot.sDrivetrain;
 
 public class CalibratePose extends SequentialCommandGroup {
 
     public CalibratePose() {
         addCommands(
-                new InstantCommand(() -> LimelightHelper.setLEDMode(true)),
-                new WaitCommand(kLimelightLEDWaitTimeSeconds),
-                new InstantCommand(this::calibratePose),
-                new InstantCommand(() -> LimelightHelper.setLEDMode(false))
+                new InstantCommand(this::calibratePose)
         );
     }
 
