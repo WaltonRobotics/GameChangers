28a855ce66d889f89bd3b34f2bf9034c4239a379
vmalepati1 <malepativikas@gmail.com>
2021-07-01 13:13:38 -0400

    Prefix trajectories with class names to prevent discrepancy

--- GameChangersRobot/src/main/java/frc/robot/auton/AutonRoutine.java
+++ GameChangersRobot/src/main/java/frc/robot/auton/AutonRoutine.java
@@ -1,6 +1,5 @@
 package frc.robot.auton;
 
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Paths;
 import frc.robot.commands.auton.RamseteTrackingCommand;
@@ -14,21 +13,9 @@ import frc.robot.commands.tuning.FindAngularMaxVelAccel;
 import frc.robot.commands.tuning.FindLinearMaxVelAccel;
 import frc.robot.commands.tuning.FindTurretAngularMaxVelAccel;
 import frc.robot.subsystems.SubsystemFlags;
-import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 
-import static frc.robot.Paths.MiscellaneousTrajectories.sTestTrajectory;
-import static frc.robot.Paths.RoutineFive.*;
-import static frc.robot.Paths.RoutineFour.*;
-import static frc.robot.Paths.RoutineOne.sBackupToShootThree;
-import static frc.robot.Paths.RoutineOne.sPickupThreeFromTrench;
-import static frc.robot.Paths.RoutineThree.sBackupToShootFive;
-import static frc.robot.Paths.RoutineThree.sPickupFiveFromTrench;
-import static frc.robot.Paths.RoutineTwo.*;
-import static frc.robot.Paths.RoutineZero.sBackwards;
-import static frc.robot.Paths.RoutineZero.sForwards;
 import static frc.robot.Robot.sDrivetrain;
 import static frc.robot.Robot.sTurret;
-
 
 public enum AutonRoutine {
 
@@ -36,30 +23,30 @@ public enum AutonRoutine {
 
     ROUTINE_ZERO_A("Cross Baseline Forwards", new SequentialCommandGroup(
             new InstantCommand(() -> AutonFlags.getInstance().setIsAutonTurretZeroingEnabled(true)),
-            new ResetPose(sForwards),
-            new RamseteTrackingCommand(sForwards, true, false)
+            new ResetPose(Paths.RoutineZero.sForwards),
+            new RamseteTrackingCommand(Paths.RoutineZero.sForwards, true, false)
     )),
 
     ROUTINE_ZERO_B("Cross Baseline Backwards", new SequentialCommandGroup(
             new InstantCommand(() -> AutonFlags.getInstance().setIsAutonTurretZeroingEnabled(true)),
-            new ResetPose(sBackwards),
-            new RamseteTrackingCommand(sBackwards, true, false)
+            new ResetPose(Paths.RoutineZero.sBackwards),
+            new RamseteTrackingCommand(Paths.RoutineZero.sBackwards, true, false)
     )),
 
     ROUTINE_ZERO_C("Shoot 3, Cross Baseline Forwards", new SequentialCommandGroup(
             new AlignTurret(1.5),
             new ShootAllBalls(3, 7.5),
             new InstantCommand(() -> AutonFlags.getInstance().setIsAutonTurretZeroingEnabled(true)),
-            new ResetPose(sForwards),
-            new RamseteTrackingCommand(sForwards, true, false)
+            new ResetPose(Paths.RoutineZero.sForwards),
+            new RamseteTrackingCommand(Paths.RoutineZero.sForwards, true, false)
     )),
 
     ROUTINE_ZERO_D("Shoot 3, Cross Baseline Backwards", new SequentialCommandGroup(
             new AlignTurret(1.5),
             new ShootAllBalls(3, 7.5),
             new InstantCommand(() -> AutonFlags.getInstance().setIsAutonTurretZeroingEnabled(true)),
-            new ResetPose(sBackwards),
-            new RamseteTrackingCommand(sBackwards, true, false)
+            new ResetPose(Paths.RoutineZero.sBackwards),
+            new RamseteTrackingCommand(Paths.RoutineZero.sBackwards, true, false)
     )),
 
     // 13
@@ -80,14 +67,14 @@ public enum AutonRoutine {
             ),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-            new ResetPose(sPickupThreeFromTrench),
-            new RamseteTrackingCommand(sPickupThreeFromTrench, true, false),
+            new ResetPose(Paths.RoutineOne.sPickupThreeFromTrench),
+            new RamseteTrackingCommand(Paths.RoutineOne.sPickupThreeFromTrench, true, false),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(false)),
             new ParallelDeadlineGroup(
-                    new RamseteTrackingCommand(sBackupToShootThree, true, false),
+                    new RamseteTrackingCommand(Paths.RoutineOne.sBackupToShootThree, true, false),
                     new SequentialCommandGroup(
-                            new WaitCommand(sBackupToShootThree.getTotalTimeSeconds() * 0.7),
+                            new WaitCommand(Paths.RoutineOne.sBackupToShootThree.getTotalTimeSeconds() * 0.7),
                             new InstantCommand(()
                                     -> AutonFlags.getInstance().setDoesAutonNeedToAlignTurretFieldRelative(true))
                     )
@@ -106,9 +93,9 @@ public enum AutonRoutine {
 
     ROUTINE_TWO_A("Pickup 2 from Enemy Trench, Shoot 5 (6pt)", new SequentialCommandGroup(
             new InstantCommand(() -> AutonFlags.getInstance().setIsAutonTurretZeroingEnabled(true)),
-            new ResetPose(sPickupTwoFromEnemyTrench),
+            new ResetPose(Paths.RoutineTwo.sPickupTwoFromEnemyTrench),
             new ParallelCommandGroup(
-                    new RamseteTrackingCommand(sPickupTwoFromEnemyTrench, true, false),
+                    new RamseteTrackingCommand(Paths.RoutineTwo.sPickupTwoFromEnemyTrench, true, false),
                     new SequentialCommandGroup(
                             new SetIntakeToggle(true),
                             new InstantCommand(() ->
@@ -116,9 +103,9 @@ public enum AutonRoutine {
                     )
             ),
             new ParallelDeadlineGroup(
-                    new RamseteTrackingCommand(sBackupToShootForSix, true, false),
+                    new RamseteTrackingCommand(Paths.RoutineTwo.sBackupToShootForSix, true, false),
                     new SequentialCommandGroup(
-                            new WaitCommand(sBackupToShootForSix.getTotalTimeSeconds() * 0.5),
+                            new WaitCommand(Paths.RoutineTwo.sBackupToShootForSix.getTotalTimeSeconds() * 0.5),
                             new InstantCommand(()
                                     -> AutonFlags.getInstance().setDoesAutonNeedToAlignTurretFieldRelative(true)),
                             new InstantCommand(() ->
@@ -137,10 +124,10 @@ public enum AutonRoutine {
             new SetIntakeToggle(true),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-            new ResetPose(sPickupTwoFromEnemyTrench),
-            new RamseteTrackingCommand(sPickupTwoFromEnemyTrench, true, false),
+            new ResetPose(Paths.RoutineTwo.sPickupTwoFromEnemyTrench),
+            new RamseteTrackingCommand(Paths.RoutineTwo.sPickupTwoFromEnemyTrench, true, false),
             new ParallelDeadlineGroup(
-                    new RamseteTrackingCommand(sBackupToShootForFour, true, false),
+                    new RamseteTrackingCommand(Paths.RoutineTwo.sBackupToShootForFour, true, false),
                     new SequentialCommandGroup(
                             new WaitCommand(3.0),
                             new InstantCommand(() ->
@@ -164,11 +151,11 @@ public enum AutonRoutine {
             ),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-            new ResetPose(sPickupFiveFromTrench),
-            new RamseteTrackingCommand(sPickupFiveFromTrench, true, false),
+            new ResetPose(Paths.RoutineThree.sPickupFiveFromTrench),
+            new RamseteTrackingCommand(Paths.RoutineThree.sPickupFiveFromTrench, true, false),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(false)),
-            new RamseteTrackingCommand(sBackupToShootFive, true, false),
+            new RamseteTrackingCommand(Paths.RoutineThree.sBackupToShootFive, true, false),
             new InstantCommand(() -> sDrivetrain.setDutyCycles(0.0, 0.0)),
             new AlignTurret(1.5),
             new ShootAllBalls(5, 4)
@@ -187,17 +174,17 @@ public enum AutonRoutine {
                     ),
                     new InstantCommand(() ->
                             AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-                    new ResetPose(sPickupThreeFromTrench),
-                    new RamseteTrackingCommand(sPickupThreeFromTrench, true, false),
+                    new ResetPose(Paths.RoutineOne.sPickupThreeFromTrench),
+                    new RamseteTrackingCommand(Paths.RoutineOne.sPickupThreeFromTrench, true, false),
                     new InstantCommand(() ->
                             AutonFlags.getInstance().setDoesAutonNeedToIntake(false)),
-                    new RamseteTrackingCommand(sBackupToAlignIntake, true, false),
+                    new RamseteTrackingCommand(Paths.RoutineFour.sBackupToAlignIntake, true, false),
                     new InstantCommand(() ->
                             AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-                    new RamseteTrackingCommand(sGoIntoRendezvousForTwo, true, false),
+                    new RamseteTrackingCommand(Paths.RoutineFour.sGoIntoRendezvousForTwo, true, false),
                     new InstantCommand(() ->
                             AutonFlags.getInstance().setDoesAutonNeedToIntake(false)),
-                    new RamseteTrackingCommand(sBackupToShoot, true, false),
+                    new RamseteTrackingCommand(Paths.RoutineFour.sBackupToShoot, true, false),
                     new InstantCommand(() -> sDrivetrain.setDutyCycles(0.0, 0.0)),
                     new AlignTurret(1.5),
                     new ShootAllBalls(5, 4)
@@ -209,8 +196,8 @@ public enum AutonRoutine {
             new SetIntakeToggle(true),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-            new ResetPose(sPickupTwoInRendezvous),
-            new RamseteTrackingCommand(sPickupTwoInRendezvous, true, false),
+            new ResetPose(Paths.RoutineFive.sPickupTwoInRendezvous),
+            new RamseteTrackingCommand(Paths.RoutineFive.sPickupTwoInRendezvous, true, false),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(false)),
             new RamseteTrackingCommand(Paths.RoutineFive.sBackupToShootFive, true, false),
@@ -231,11 +218,11 @@ public enum AutonRoutine {
             ),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-            new ResetPose(sPickupFourInRendezvous),
-            new RamseteTrackingCommand(sPickupFourInRendezvous, true, false),
+            new ResetPose(Paths.RoutineFive.sPickupFourInRendezvous),
+            new RamseteTrackingCommand(Paths.RoutineFive.sPickupFourInRendezvous, true, false),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(false)),
-            new RamseteTrackingCommand(sBackupToShootFour, true, false),
+            new RamseteTrackingCommand(Paths.RoutineFive.sBackupToShootFour, true, false),
             new InstantCommand(() -> sDrivetrain.setDutyCycles(0.0, 0.0)),
             new AlignTurret(1.5),
             new ShootAllBalls(4, 4)
@@ -246,11 +233,11 @@ public enum AutonRoutine {
             new SetIntakeToggle(true),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(true)),
-            new ResetPose(sPickupTwoFromEnemyTrench),
-            new RamseteTrackingCommand(sPickupTwoFromEnemyTrench, true, false),
+            new ResetPose(Paths.RoutineTwo.sPickupTwoFromEnemyTrench),
+            new RamseteTrackingCommand(Paths.RoutineTwo.sPickupTwoFromEnemyTrench, true, false),
             new InstantCommand(() ->
                     AutonFlags.getInstance().setDoesAutonNeedToIntake(false)),
-            new RamseteTrackingCommand(sBackupToShootForSix, true, false),
+            new RamseteTrackingCommand(Paths.RoutineTwo.sBackupToShootForSix, true, false),
             new InstantCommand(() -> sDrivetrain.setDutyCycles(0.0, 0.0)),
             new AlignTurret(1.5),
             new ShootAllBalls(5, 4),
@@ -307,8 +294,8 @@ public enum AutonRoutine {
 
     TEST_TRAJECTORY("Simple Spline",
             new SequentialCommandGroup(
-                    new ResetPose(sTestTrajectory),
-                    new RamseteTrackingCommand(sTestTrajectory, true, false)
+                    new ResetPose(Paths.MiscellaneousTrajectories.sTestTrajectory),
+                    new RamseteTrackingCommand(Paths.MiscellaneousTrajectories.sTestTrajectory, true, false)
             )
     );
 
